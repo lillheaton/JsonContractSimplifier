2d69e0bdc5ecdac66e79bf43cd5e286c6d69ddb9
Emil Olsson <emilolsson93@gmail.com>
2019-04-16 16:08:56 +0200

    Remove function from IConverterLocatorService

--- src/Services/ConverterLocator/IConverterLocatorService.cs
+++ src/Services/ConverterLocator/IConverterLocatorService.cs
@@ -4,7 +4,6 @@ namespace JsonContractSimplifier.Services.ConverterLocator
 {
     public interface IConverterLocatorService
     {
-        Type[] LoadConverters();
         bool TryFindConverterFor(Type type, out IConverter converter);
         object Convert(object target, IConverter converter);
     }
